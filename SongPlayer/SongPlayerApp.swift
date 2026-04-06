e4fb0d431c3ddebb3c787662b0d61677d3e15c14
Joao Barros <joaopaulofariasantos@gmail.com>
2026-04-06 01:49:18 -0300

    initial setup

---
+++ SongPlayer/SongPlayerApp.swift
@@ -0,0 +1,32 @@
+//
+//  SongPlayerApp.swift
+//  SongPlayer
+//
+//  Created by Joao Barros on 06/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SongPlayerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SongPlayerUITests/SongPlayerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SongPlayerUITestsLaunchTests.swift
+//  SongPlayerUITests
+//
+//  Created by Joao Barros on 06/04/26.
+//
+
+import XCTest
+
+final class SongPlayerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
